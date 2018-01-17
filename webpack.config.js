5412653131fa9ff44754bcc955fb6064ad78b467
Galatea de Thebes <270696@mail.ru>
2018-01-17 17:04:47 +0700

    file-loader html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,24 @@ module.exports = {
   },
   module: {
     loaders: [
+      {
+  test: /\.(html)$/,
+  use: {
+    loader: 'html-loader',
+    options: {
+      attrs: [':data-src']
+    }
+  }
+},
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {}
+          }
+        ]
+      },
       { test: /\.less$/,
           use: [{
                   loader: "style-loader" // creates style nodes from JS strings
